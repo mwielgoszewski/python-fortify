d6a4eb51a369f705634c61d1496b028d52cc377c
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2014-09-13 22:33:46 -0400

    new FPR class

--- fortify/fpr.py
+++ fortify/fpr.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 '''
-fortify
-~~~~~~~
+fortify.fpr
+~~~~~~~~~~~
 
 '''
+from .utils import openfpr
+
 
 class FPR(object):
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, project, **kwargs):
+        if isinstance(project, basestring):
+            self._project = project = openfpr(project)
+        elif isinstance(package, dict):
+            self._project = project
+        else:
+            raise TypeError
+
+        self.FVDL = project['audit.fvdl'].getroot()
+        self.Audit = project['audit.xml'].getroot()
+        self.FilterTemplate = project['filtertemplate.xml'].getroot()
